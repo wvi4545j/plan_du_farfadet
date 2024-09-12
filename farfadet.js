4f849b5200fa20070265786bc2b4342e53b52472
wvi4545j <k9um96e3@outlook.com>
2024-09-12 14:37:03 +0200

    Update farfadet.js

--- farfadet.js
+++ farfadet.js
@@ -1,6 +1,13 @@
-((a,b)=>{
-  document.body.append(a);b.forEach(c=>{a.style[c[0]]=c[1];});
+((a,b,c,d)=>{
+  var e='';c.split('--link--').forEach((f,g)=>{e+=f+(d[g]?d[g]:'');});
+  document.body.append(a);a.setAttribute('class','farfadet');b.textContent=e;document.head.append(b);
 })(
  document.createElement('div'),
- [['background','#000'],['bottom','calc(50vh - 150px)'],['height','350px'],['left','20px'],['position','absolute'],['width','150px']]
+ document.createElement('style'),
+ `
+ .farfadet{bottom:calc(50vh - 150px);height:350px;left:20px;position:absolute;width:width:150px;}
+ .farfadet::before{background:url(--link--) no-repeat 100%;background-size:auto 15px;content:'Ads by ';display:block;font:16px Arial;height:20px;}
+ `,[
+   ''
+ ]
 );
